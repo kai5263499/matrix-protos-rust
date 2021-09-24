70ed80bd4206f58083b4fbb22ffb549ad559f2de
Wes Widner <wes@manwe.io>
2021-09-24 06:11:58 -0400

    Fiddling with github action

--- examples/serialize.rs
+++
@@ -1,9 +0,0 @@
-extern crate matrix_protos_rust;
-
-use matrix_protos_rust::protos::io::EverloopImage;
-
-fn main() { 
-    let mut eli = EverloopImage::new();
-    eli.set_everloop_length(18);
-    println!("eli:{:?}", eli);
-}
